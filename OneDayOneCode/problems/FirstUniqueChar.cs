11396fc4082cb352654c35fcf72861eefa4649e9
yulifrank <148456398+yulifrank@users.noreply.github.com>
2025-06-29 22:26:29 +0300

    add  FirstUniqueChar file with solution

---
+++ OneDayOneCode/problems/FirstUniqueChar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneDayOneCode.problems
+{
+    internal class FirstUniqueChar
+    {
+
+        public static char FindFirstUniqueChar(string input)
+        {
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            foreach (char c in input)
+            {
+                if (counts.ContainsKey(c))
+                    counts[c]++;
+                else
+                {
+                    counts[c] = 1;
+                }
+            }
+            foreach(var  o in counts)
+            {
+                if(o.Value==1)
+                    return o.Key;
+            }
+            return '0';
+
+        }
+    }
+}
